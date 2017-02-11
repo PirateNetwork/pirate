37a6ce872e65bad37a77758257266351b9de6e38
Zero <zerocryptocurrency@gmail.com>
2017-02-11 19:56:07 +0100

    Reset checkpoints

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -121,14 +121,10 @@ public:
 
         checkpointData = (Checkpoints::CCheckpointData) {
             boost::assign::map_list_of
-            (0, consensus.hashGenesisBlock)
-            (2500, uint256S("0x00000006dc968f600be11a86cbfbf7feb61c7577f45caced2e82b6d261d19744"))
-            (15000, uint256S("0x00000000b6bc56656812a5b8dcad69d6ad4446dec23b5ec456c18641fb5381ba")),
-            1479851027,     // * UNIX timestamp of last checkpoint block
-            110415,         // * total number of transactions between genesis and last checkpoint
-                            //   (the tx=... number in the SetBestChain debug.log lines)
-            4240            // * estimated number of transactions per day after checkpoint
-                            //   total number of tx / (checkpoint block height / (24 * 24))
+            ( 0, consensus.hashGenesisBlock),
+            genesis.nTime,
+            0,
+            0
         };
 
     }
@@ -250,8 +246,8 @@ public:
 
         checkpointData = (Checkpoints::CCheckpointData){
             boost::assign::map_list_of
-            ( 0, uint256S("0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206")),
-            0,
+            ( 0, consensus.hashGenesisBlock),
+            genesis.nTime,
             0,
             0
         };
