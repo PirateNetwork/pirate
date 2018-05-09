acdd35d2f7e09be837e6102a93b4e7f8ccaa6270
Walter Karshat <wkarshat@yahoo.com>
2018-05-08 23:16:22 -0700

    Update version strings to 1.0.14-1 and change the name Change DEPRECATION to 26 weeks

--- src/deprecation.h
+++ src/deprecation.h
@@ -6,16 +6,16 @@
 #define ZCASH_DEPRECATION_H
 
 // Deprecation policy:
-// * Shut down 16 weeks' worth of blocks after the estimated release block height.
-// * A warning is shown during the 2 weeks' worth of blocks prior to shut down.
-static const int APPROX_RELEASE_HEIGHT = 249000;
-static const int WEEKS_UNTIL_DEPRECATION = 16;
+// * Shut down 26 weeks' worth of blocks after the estimated release block height.
+// * A warning is shown during the 4 weeks' worth of blocks prior to shut down.
+static const int APPROX_RELEASE_HEIGHT = 315000;
+static const int WEEKS_UNTIL_DEPRECATION = 26;
 //Fixing zero day size
 static const int DEPRECATION_HEIGHT = APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 24 * 30);
 
 // Number of blocks before deprecation to warn users
 //Fixing zero day size
-static const int DEPRECATION_WARN_LIMIT = 14 * 24 * 30; // 2 weeks
+static const int DEPRECATION_WARN_LIMIT = 28 * 24 * 30; // 4 weeks
 
 /**
  * Checks whether the node is deprecated based on the current block height, and
