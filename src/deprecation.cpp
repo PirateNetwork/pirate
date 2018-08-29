1e3959b9d5971e4419a36eb3eb793fb6bcaf3ec9
CryptoForge <cryptoforge.cc@gmail.com>
2018-08-28 21:08:05 -0700

    remove -disabledeprecation flag

--- src/deprecation.cpp
+++ src/deprecation.cpp
@@ -8,12 +8,17 @@
 #include "init.h"
 #include "ui_interface.h"
 #include "util.h"
+#include "chainparams.h"
 
 static const std::string CLIENT_VERSION_STR = FormatVersion(CLIENT_VERSION);
 
 void EnforceNodeDeprecation(int nHeight, bool forceLogging) {
+
+    // Do not enforce deprecation in regtest or on testnet
+    std::string networkID = Params().NetworkIDString();
+    if (networkID != "main") return;
+
     int blocksToDeprecation = DEPRECATION_HEIGHT - nHeight;
-    bool disableDeprecation = (GetArg("-disabledeprecation", "") == CLIENT_VERSION_STR);
     if (blocksToDeprecation <= 0) {
         // In order to ensure we only log once per process when deprecation is
         // disabled (to avoid log spam), we only need to log in two cases:
@@ -24,31 +29,16 @@ void EnforceNodeDeprecation(int nHeight, bool forceLogging) {
         if (blocksToDeprecation == 0 || forceLogging) {
             auto msg = strprintf(_("This version has been deprecated as of block height %d."),
                                  DEPRECATION_HEIGHT) + " " +
-                       _("You should upgrade to the latest version of Zcash.");
-            if (!disableDeprecation) {
-                msg += " " + strprintf(_("To disable deprecation for this version, set %s%s."),
-                                       "-disabledeprecation=", CLIENT_VERSION_STR);
-            }
+                       _("You should upgrade to the latest version of ZERO.");
             LogPrintf("*** %s\n", msg);
             uiInterface.ThreadSafeMessageBox(msg, "", CClientUIInterface::MSG_ERROR);
         }
-        if (!disableDeprecation) {
-            StartShutdown();
-        }
+        StartShutdown();
     } else if (blocksToDeprecation == DEPRECATION_WARN_LIMIT ||
                (blocksToDeprecation < DEPRECATION_WARN_LIMIT && forceLogging)) {
-        std::string msg;
-        if (disableDeprecation) {
-            msg = strprintf(_("This version will be deprecated at block height %d."),
+        std::string msg = strprintf(_("This version will be deprecated at block height %d, and will automatically shut down."),
                             DEPRECATION_HEIGHT) + " " +
-                  _("You should upgrade to the latest version of Zcash.");
-        } else {
-            msg = strprintf(_("This version will be deprecated at block height %d, and will automatically shut down."),
-                            DEPRECATION_HEIGHT) + " " +
-                  _("You should upgrade to the latest version of Zcash.") + " " +
-                  strprintf(_("To disable deprecation for this version, set %s%s."),
-                            "-disabledeprecation=", CLIENT_VERSION_STR);
-        }
+                  _("You should upgrade to the latest version of ZERO.");
         LogPrintf("*** %s\n", msg);
         uiInterface.ThreadSafeMessageBox(msg, "", CClientUIInterface::MSG_WARNING);
     }
