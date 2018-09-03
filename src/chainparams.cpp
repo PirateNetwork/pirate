ff4f8c6a54949046104b987ae99005a03fd7717b
cryptoforge <cryptoforge.cc@gmail.com>
2018-09-03 11:39:37 -0700

    Update mainnet DNS seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -107,9 +107,11 @@ public:
 
         vFixedSeeds.clear();
         vSeeds.clear();
-        //vSeeds.push_back(CDNSSeedData("z.cash", "dnsseed.z.cash")); // Zcash
-        //vSeeds.push_back(CDNSSeedData("str4d.xyz", "dnsseed.str4d.xyz")); // @str4d
-        //vSeeds.push_back(CDNSSeedData("znodes.org", "dnsseed.znodes.org")); // @bitcartel
+        vSeeds.push_back(CDNSSeedData("CryptoForge", "zeroseed.cryptoforge.cc")); // @cryptoforge
+        vSeeds.push_back(CDNSSeedData("prophetalgorithms", "zerocurrency.prophetalgorithms.com")); // @MoodyDreamer
+        vSeeds.push_back(CDNSSeedData("cryptonode1", "zseed1.cryptonode.cloud")); // @Kryptostuff
+        vSeeds.push_back(CDNSSeedData("cryptonode2", "zseed2.cryptonode.cloud")); // @bKRyptostuff
+        vSeeds.push_back(CDNSSeedData("zerocurrency", "insight.zerocurrency.io")); // @R3DEY3
 
         // guarantees the first 2 characters, when base58 encoded, are "t1"
         base58Prefixes[PUBKEY_ADDRESS]     = {0x1C,0xB8};
