4f9f2f1ffdc7458e9441192e088f46e80c9f971a
cryptoforge <cryptoforge.cc@gmail.com>
2018-10-26 21:26:17 -0700

    update dbchar for TIMESTAMPINDEX & BLOCKHASHINDEX

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -28,8 +28,8 @@ static const char DB_BLOCK_FILES = 'f';
 static const char DB_TXINDEX = 't';
 static const char DB_ADDRESSINDEX = 'd';
 static const char DB_ADDRESSUNSPENTINDEX = 'u';
-static const char DB_TIMESTAMPINDEX = 'S';
-static const char DB_BLOCKHASHINDEX = 'z';
+static const char DB_TIMESTAMPINDEX = 'T'; //changed from S
+static const char DB_BLOCKHASHINDEX = 'h'; //changed from z
 static const char DB_SPENTINDEX = 'p';
 static const char DB_BLOCK_INDEX = 'b';
 
