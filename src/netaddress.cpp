131fff71ca5d7baecb2b70ab56771f1315316149
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2020-08-07 19:55:51 +0200

    util: make EncodeBase32 consume Spans

--- src/netaddress.cpp
+++ src/netaddress.cpp
@@ -8,10 +8,11 @@
 #endif
 
 #include "netaddress.h"
+
 #include "hash.h"
+#include "tinyformat.h"
 #include "util/strencodings.h"
 #include "util/asmap.h"
-#include "tinyformat.h"
 
 #include <algorithm>
 #include <array>
@@ -344,9 +345,9 @@ enum Network CNetAddr::GetNetwork() const
 std::string CNetAddr::ToStringIP() const
 {
     if (IsTor())
-        return EncodeBase32(m_addr.data(), m_addr.size()) + ".onion";
+        return EncodeBase32(m_addr) + ".onion";
     if (IsInternal())
-        return EncodeBase32(m_addr.data(), m_addr.size()) + ".internal";
+        return EncodeBase32(m_addr) + ".internal";
     CService serv(*this, 0);
     struct sockaddr_storage sockaddr;
     socklen_t socklen = sizeof(sockaddr);
