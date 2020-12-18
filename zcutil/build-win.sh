f3d2b74db9db92ed798eae7af67b66bd64f99790
Cryptoforge <cryptoforge.cc@gmail.com>
2020-12-18 11:49:41 -0800

    change daemon name to pirate

--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -22,4 +22,4 @@ cd ../
 CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site CXXFLAGS="-DPTW32_STATIC_LIB -DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread" ./configure --prefix="${PREFIX}" --host=x86_64-w64-mingw32 --enable-static --disable-shared
 sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' configure
 cd src/
-CC="${CC} -g " CXX="${CXX} -g " make V=1  komodod.exe komodo-cli.exe komodo-tx.exe
+CC="${CC} -g " CXX="${CXX} -g " make V=1  pirated.exe pirate-cli.exe pirate-tx.exe
