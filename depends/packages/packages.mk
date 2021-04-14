d5af8e01d50626e315ce3dcca7c607c2c4cc8efe
Cryptoforge <cryptoforge.cc@gmail.com>
2021-04-13 20:21:51 -0700

    Update Librustzcash and build system

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,5 +1,3 @@
-rust_packages := rust librustzcash
-
 ifeq ($(build_os),darwin)
 	zcash_packages := libsnark libgmp libsodium utfcpp
 else
@@ -7,42 +5,7 @@ else
 	zcash_packages := libgmp libsodium utfcpp
 endif
 
-rust_crates := \
-  crate_aes \
-  crate_aesni \
-  crate_aes_soft \
-  crate_arrayvec \
-  crate_bitflags \
-  crate_bit_vec \
-  crate_blake2_rfc \
-  crate_block_cipher_trait \
-  crate_byte_tools \
-  crate_byteorder \
-  crate_constant_time_eq \
-  crate_crossbeam \
-  crate_digest \
-  crate_fpe \
-  crate_fuchsia_zircon \
-  crate_fuchsia_zircon_sys \
-  crate_futures_cpupool \
-  crate_futures \
-  crate_generic_array \
-  crate_lazy_static \
-  crate_libc \
-  crate_nodrop \
-  crate_num_bigint \
-  crate_num_cpus \
-  crate_num_integer \
-  crate_num_traits \
-  crate_opaque_debug \
-  crate_rand \
-  crate_stream_cipher \
-  crate_typenum \
-  crate_winapi_i686_pc_windows_gnu \
-  crate_winapi \
-  crate_winapi_x86_64_pc_windows_gnu
-rust_packages := rust $(rust_crates) librustzcash
-native_packages := native_ccache
+native_packages := native_ccache native_rust
 
 wallet_packages=bdb
 
@@ -51,4 +14,3 @@ ifeq ($(host_os),linux)
 else
 	packages := boost openssl libevent zeromq $(zcash_packages) libcurl googletest #googlemock
 endif
-
