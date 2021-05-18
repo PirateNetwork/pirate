48e6bd1673fcdd8a6c1944ffe257ea1f276e6b24
Mullroy <mullroy@protonmail.com>
2021-05-18 19:21:51 +0200

    Update 2 dependencies to build on Ubuntu 21.04 (g++ 10)

--- depends/packages/libgmp.mk
+++ depends/packages/libgmp.mk
@@ -1,9 +1,10 @@
 package=libgmp
 
-$(package)_version=6.1.1
+$(package)_version=6.2.1
 $(package)_download_path=https://ftp.gnu.org/gnu/gmp
 $(package)_file_name=gmp-$($(package)_version).tar.bz2
-$(package)_sha256_hash=a8109865f2893f1373b0a8ed5ff7429de8db696fc451b1036bd7bdf95bbeffd6
+$(package)_sha256_hash=eae9326beb4158c386e39a356818031bd28f3124cf915f8c5b1dc4c7a36b4d7c
+#a8109865f2893f1373b0a8ed5ff7429de8db696fc451b1036bd7bdf95bbeffd6
 $(package)_dependencies=
 $(package)_config_opts=--enable-cxx --disable-shared
 
