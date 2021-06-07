26843c9c0307abb74b3605d51a40605bd3b7a6a5
John Jones <jmjatlanta@gmail.com>
2021-06-07 10:33:08 -0500

    wrap test chain funcs into class

--- src/test-komodo/testutils.cpp
+++ src/test-komodo/testutils.cpp
@@ -65,40 +65,55 @@ void setupChain()
     InitBlockIndex();
 }
 
-
+/***
+ * Generate a block
+ * @param block a place to store the block (nullptr skips the disk read)
+ */
 void generateBlock(CBlock *block)
 {
+    SetMockTime(nMockTime+=100);  // CreateNewBlock can fail if not enough time passes
+
     UniValue params;
     params.setArray();
     params.push_back(1);
-    uint256 blockId;
-
-    SetMockTime(nMockTime+=100);  // CreateNewBlock can fail if not enough time passes
 
     try {
         UniValue out = generate(params, false, CPubKey());
+        uint256 blockId;
         blockId.SetHex(out[0].getValStr());
-        if (block) ASSERT_TRUE(ReadBlockFromDisk(*block, mapBlockIndex[blockId], false));
+        if (block) 
+            ASSERT_TRUE(ReadBlockFromDisk(*block, mapBlockIndex[blockId], false));
     } catch (const UniValue& e) {
         FAIL() << "failed to create block: " << e.write().data();
     }
 }
 
-
+/***
+ * Accept a transaction, failing the gtest if the tx is not accepted
+ * @param tx the transaction to be accepted
+ */
 void acceptTxFail(const CTransaction tx)
 {
     CValidationState state;
-    if (!acceptTx(tx, state)) FAIL() << state.GetRejectReason();
+    if (!acceptTx(tx, state)) 
+        FAIL() << state.GetRejectReason();
 }
 
 
 bool acceptTx(const CTransaction tx, CValidationState &state)
 {
     LOCK(cs_main);
-    return AcceptToMemoryPool(mempool, state, tx, false, NULL);
+    bool missingInputs = false;
+    bool accepted = AcceptToMemoryPool(mempool, state, tx, false, &missingInputs, false, -1);
+    return accepted && !missingInputs;
 }
 
-
+/***
+ * Create a transaction based on input
+ * @param txIn the vin data (which becomes prevout)
+ * @param nOut the index of txIn to use as prevout
+ * @returns the transaction
+ */
 CMutableTransaction spendTx(const CTransaction &txIn, int nOut)
 {
     CMutableTransaction mtx;
@@ -125,7 +140,7 @@ std::vector<uint8_t> getSig(const CMutableTransaction mtx, CScript inputPubKey, 
  * In order to do tests there needs to be inputs to spend.
  * This method creates a block and returns a transaction that spends the coinbase.
  */
-void getInputTx(CScript scriptPubKey, CTransaction &txIn)
+CTransaction getInputTx(CScript scriptPubKey)
 {
     // Get coinbase
     CBlock block;
@@ -143,5 +158,186 @@ void getInputTx(CScript scriptPubKey, CTransaction &txIn)
 
     // Accept
     acceptTxFail(mtx);
-    txIn = CTransaction(mtx);
+    return CTransaction(mtx);
+}
+
+/****
+ * A class to provide a simple chain for tests
+ */
+
+TestChain::TestChain()
+{
+    setupChain();
+    CBitcoinSecret vchSecret;
+    vchSecret.SetString(notarySecret); // this returns false due to network prefix mismatch but works anyway
+    notaryKey = vchSecret.GetKey();
+}
+
+CBlock TestChain::generateBlock()
+{
+    CBlock block;
+    ::generateBlock(&block);
+    for(auto wallet : toBeNotified)
+    {
+        wallet->BlockNotification(block);
+    }
+    return block;
+}
+
+CKey TestChain::getNotaryKey() { return notaryKey; }
+
+CValidationState TestChain::acceptTx(const CTransaction& tx)
+{
+    CValidationState retVal;
+    bool accepted = ::acceptTx(tx, retVal);
+    if (!accepted && retVal.IsValid())
+        retVal.DoS(100, false, 0U, "acceptTx returned false");
+    return retVal;
+}
+
+std::shared_ptr<TestWallet> TestChain::AddWallet(const CKey& in)
+{
+    std::shared_ptr<TestWallet> retVal = std::make_shared<TestWallet>(this, in);
+    toBeNotified.push_back(retVal);
+    return retVal;
+}
+
+std::shared_ptr<TestWallet> TestChain::AddWallet()
+{
+    std::shared_ptr<TestWallet> retVal = std::make_shared<TestWallet>(this);
+    toBeNotified.push_back(retVal);
+    return retVal;
+}
+
+
+/***
+ * A simplistic (dumb) wallet for helping with testing
+ * - It does not keep track of spent transactions
+ * - Blocks containing vOuts that apply are added to the front of a vector
+ */
+
+TestWallet::TestWallet(TestChain* chain) : chain(chain)
+{
+    key.MakeNewKey(true);
+    destScript = GetScriptForDestination(key.GetPubKey());
+}
+
+TestWallet::TestWallet(TestChain* chain, const CKey& in) : chain(chain), key(in)
+{
+    destScript = GetScriptForDestination(key.GetPubKey());
+}
+
+/***
+ * @returns the public key
+ */
+CPubKey TestWallet::GetPubKey() const { return key.GetPubKey(); }
+
+/***
+ * @returns the private key
+ */
+CKey TestWallet::GetPrivKey() const { return key; }
+
+/***
+ * Sign a typical transaction
+ * @param hash the hash to sign
+ * @param hashType SIGHASH_ALL or something similar
+ * @returns the bytes to add to ScriptSig
+ */
+std::vector<unsigned char> TestWallet::Sign(uint256 hash, unsigned char hashType)
+{
+    std::vector<unsigned char> retVal;
+    key.Sign(hash, retVal);
+    retVal.push_back(hashType);
+    return retVal;
+}
+
+/***
+ * Sign a cryptocondition
+ * @param cc the cryptocondition
+ * @param hash the hash to sign
+ * @returns the bytes to add to ScriptSig
+ */
+std::vector<unsigned char> TestWallet::Sign(CC* cc, uint256 hash)
+{
+    int out = cc_signTreeSecp256k1Msg32(cc, key.begin(), hash.begin());            
+    return CCSigVec(cc);
+}
+
+/***
+ * Notifies this wallet of a new block
+ */
+void TestWallet::BlockNotification(const CBlock& block)
+{
+    // TODO: remove spent txs from availableTransactions
+    // see if this block has any outs for me
+    for( auto tx : block.vtx )
+    {
+        for(uint32_t i = 0; i < tx.vout.size(); ++i)
+        {
+            if (tx.vout[i].scriptPubKey == destScript)
+            {
+                availableTransactions.insert(availableTransactions.begin(), std::pair<CTransaction, uint32_t>(tx, i));
+                break; // skip to next tx
+            }
+        }
+    }
+}
+
+/***
+ * Get a transaction that has funds
+ * NOTE: If no single transaction matches, throws
+ * @param needed how much is needed
+ * @returns a pair of CTransaction and the n value of the vout
+ */
+std::pair<CTransaction, uint32_t> TestWallet::GetAvailable(CAmount needed)
+{
+    for(auto txp : availableTransactions)
+    {
+        CTransaction tx = txp.first;
+        uint32_t n = txp.second;
+        if (tx.vout[n].nValue >= needed)
+            return txp;
+    }
+    throw std::logic_error("No Funds");
+}
+
+/***
+ * Add a transaction to the list of available vouts
+ * @param tx the transaction
+ * @param n the n value of the vout
+ */
+void TestWallet::AddOut(CTransaction tx, uint32_t n)
+{
+    availableTransactions.insert(availableTransactions.begin(), std::pair<CTransaction, uint32_t>(tx, n));
+}
+
+/***
+ * Transfer to another user
+ * @param to who to transfer to
+ * @param amount the amount
+ * @returns the results
+ */
+CValidationState TestWallet::Transfer(std::shared_ptr<TestWallet> to, CAmount amount, CAmount fee)
+{
+    std::pair<CTransaction, uint32_t> available = GetAvailable(amount + fee);
+    CMutableTransaction tx;
+    CTxIn incoming;
+    incoming.prevout.hash = available.first.GetHash();
+    incoming.prevout.n = available.second;
+    tx.vin.push_back(incoming);
+    CTxOut out1;
+    out1.scriptPubKey = GetScriptForDestination(to->GetPubKey());
+    out1.nValue = amount;
+    tx.vout.push_back(out1);
+    // give the rest back to the notary
+    CTxOut out2;
+    out2.scriptPubKey = GetScriptForDestination(key.GetPubKey());
+    out2.nValue = available.first.vout[available.second].nValue - amount - fee;
+    tx.vout.push_back(out2);
+
+    uint256 hash = SignatureHash(available.first.vout[available.second].scriptPubKey, tx, 0, SIGHASH_ALL, 0, 0);
+    tx.vin[0].scriptSig << Sign(hash, SIGHASH_ALL);
+
+    CTransaction fundTo(tx);
+    return chain->acceptTx(fundTo);
 }
--- src/test-komodo/testutils.h
+++ src/test-komodo/testutils.h
@@ -1,8 +1,6 @@
-#ifndef TESTUTILS_H
-#define TESTUTILS_H
+#pragma once
 
 #include "main.h"
-
 
 #define VCH(a,b) std::vector<unsigned char>(a, a + b)
 
@@ -18,12 +16,122 @@ extern CKey notaryKey;
 
 
 void setupChain();
+/***
+ * Generate a block
+ * @param block a place to store the block (read from disk)
+ */
 void generateBlock(CBlock *block=NULL);
 bool acceptTx(const CTransaction tx, CValidationState &state);
 void acceptTxFail(const CTransaction tx);
-void getInputTx(CScript scriptPubKey, CTransaction &txIn);
+/****
+ * In order to do tests there needs to be inputs to spend.
+ * This method creates a block and returns a transaction that spends the coinbase.
+ * @param scriptPubKey
+ * @returns the transaction
+ */
+CTransaction getInputTx(CScript scriptPubKey);
 CMutableTransaction spendTx(const CTransaction &txIn, int nOut=0);
 std::vector<uint8_t> getSig(const CMutableTransaction mtx, CScript inputPubKey, int nIn=0);
 
 
-#endif /* TESTUTILS_H */
+class TestWallet;
+
+class TestChain
+{
+public:
+    /***
+     * ctor to create a chain
+     */
+    TestChain();
+    /**
+     * Generate a block
+     * @returns the block generated
+     */
+    CBlock generateBlock();
+    /***
+     * @returns the notary's key
+     */
+    CKey getNotaryKey();
+    /***
+     * Add a transactoion to the mempool
+     * @param tx the transaction
+     * @returns the results
+     */
+    CValidationState acceptTx(const CTransaction &tx);
+    /***
+     * Creates a wallet with a specific key
+     * @param key the key
+     * @returns the wallet
+     */
+    std::shared_ptr<TestWallet> AddWallet(const CKey &key);
+    /****
+     * Create a wallet
+     * @returns the wallet
+     */
+    std::shared_ptr<TestWallet> AddWallet();
+private:
+    std::vector<std::shared_ptr<TestWallet>> toBeNotified;
+};
+
+/***
+ * A simplistic (dumb) wallet for helping with testing
+ * - It does not keep track of spent transactions
+ * - Blocks containing vOuts that apply are added to the front of a vector
+ */
+class TestWallet
+{
+public:
+    TestWallet(TestChain* chain);
+    TestWallet(TestChain* chain, const CKey& in);
+    /***
+     * @returns the public key
+     */
+    CPubKey GetPubKey() const;
+    /***
+     * @returns the private key
+     */
+    CKey GetPrivKey() const;
+    /***
+     * Sign a typical transaction
+     * @param hash the hash to sign
+     * @param hashType SIGHASH_ALL or something similar
+     * @returns the bytes to add to ScriptSig
+     */
+    std::vector<unsigned char> Sign(uint256 hash, unsigned char hashType);
+    /***
+     * Sign a cryptocondition
+     * @param cc the cryptocondition
+     * @param hash the hash to sign
+     * @returns the bytes to add to ScriptSig
+     */
+    std::vector<unsigned char> Sign(CC* cc, uint256 hash);
+    /***
+     * Notifies this wallet of a new block
+     */
+    void BlockNotification(const CBlock& block);
+    /***
+     * Get a transaction that has funds
+     * NOTE: If no single transaction matches, throws
+     * @param needed how much is needed
+     * @returns a pair of CTransaction and the n value of the vout
+     */
+    std::pair<CTransaction, uint32_t> GetAvailable(CAmount needed);
+    /***
+     * Add a transaction to the list of available vouts
+     * @param tx the transaction
+     * @param n the n value of the vout
+     */
+    void AddOut(CTransaction tx, uint32_t n);
+    /***
+     * Transfer to another user
+     * @param to who to transfer to
+     * @param amount the amount
+     * @returns the results
+     */
+    CValidationState Transfer(std::shared_ptr<TestWallet> to, CAmount amount, CAmount fee = 0);
+private:
+    TestChain *chain;
+    CKey key;
+    std::vector<std::pair<CTransaction, uint8_t>> availableTransactions;
+    CScript destScript;
+};
