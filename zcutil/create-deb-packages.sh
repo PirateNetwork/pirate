4a417489715a442a729e65030398ae737b54d32c
Cryptoforge <cryptoforge.cc@gmail.com>
2021-07-07 13:11:24 -0700

    fix aarch64 deb package

--- zcutil/create-deb-packages.sh
+++ zcutil/create-deb-packages.sh
@@ -1,4 +1,4 @@
-export APP_VERSION="5.1.1-1"
+export APP_VERSION="5.2.0"
 
 echo -n "Building amd64 deb..........."
 debdir=bin/pirate-qt-ubuntu1804-v$APP_VERSION
@@ -6,7 +6,7 @@ mkdir -p $debdir > /dev/null
 mkdir    $debdir/DEBIAN
 mkdir -p $debdir/usr/local/bin
 
-cat zcutil/deb/control | sed "s/RELEASE_VERSION/$APP_VERSION/g" > $debdir/DEBIAN/control
+cat zcutil/deb/control_amd64 | sed "s/RELEASE_VERSION/$APP_VERSION/g" > $debdir/DEBIAN/control
 
 cp release/pirate-qt-linux                   $debdir/usr/local/bin/pirate-qt
 
@@ -22,13 +22,13 @@ rm ./bin -rf
 echo "[OK]"
 
 
-echo -n "Building arrch64 deb..........."
+echo -n "Building aarch64 deb..........."
 debdir=bin/pirate-qt-arrch64-v$APP_VERSION
 mkdir -p $debdir > /dev/null
 mkdir    $debdir/DEBIAN
 mkdir -p $debdir/usr/local/bin
 
-cat zcutil/deb/control | sed "s/RELEASE_VERSION/$APP_VERSION/g" > $debdir/DEBIAN/control
+cat zcutil/deb/control_aarch64 | sed "s/RELEASE_VERSION/$APP_VERSION/g" > $debdir/DEBIAN/control
 
 cp release/pirate-qt-arm                   $debdir/usr/local/bin/pirate-qt
 
