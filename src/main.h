3698d27374615c3df176b941a5ca3731a98b52c9
Cryptoforge <cryptoforge.cc@gmail.com>
2021-08-24 18:40:23 -0700

    flash wallet to disk every 15 minutes

--- src/main.h
+++ src/main.h
@@ -113,7 +113,7 @@ static const unsigned int MAX_HEADERS_RESULTS = 160;
  *  harder). We'll probably want to make this a per-peer adaptive value at some point. */
 static unsigned int BLOCK_DOWNLOAD_WINDOW = 1024;
 /** Time to wait (in seconds) between writing blocks/block index to disk. */
-static const unsigned int DATABASE_WRITE_INTERVAL = 60 * 60;
+static const unsigned int DATABASE_WRITE_INTERVAL = 15 * 60;
 /** Time to wait (in seconds) between flushing chainstate to disk. */
 static const unsigned int DATABASE_FLUSH_INTERVAL = 24 * 60 * 60;
 /** Maximum length of reject messages. */
