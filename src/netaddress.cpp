d335b1813b1b03f61ced3e7117729a022a6d38e9
Cryptoforge <cryptoforge.cc@gmail.com>
2022-03-24 21:33:49 -0700

    remove some unused functions

--- src/netaddress.cpp
+++ src/netaddress.cpp
@@ -683,11 +683,6 @@ std::string CService::ToString() const
     return ToStringIPPort();
 }
 
-void CService::SetPort(unsigned short portIn)
-{
-    port = portIn;
-}
-
 CSubNet::CSubNet():
     valid(false)
 {
