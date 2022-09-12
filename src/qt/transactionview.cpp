63813f0ae2209589599ccc99d203ec70b6d78e6c
Mullroy <mullroy@protonmail.com>
2022-09-12 16:22:43 +0200

    Include transactions to self with memos in the search results

--- src/qt/transactionview.cpp
+++ src/qt/transactionview.cpp
@@ -92,7 +92,8 @@ TransactionView::TransactionView(const PlatformStyle *platformStyle, QWidget *pa
     typeWidget->addItem(tr("Sent to"), TransactionFilterProxy::TYPE(TransactionRecord::SendToAddress) |
                                        TransactionFilterProxy::TYPE(TransactionRecord::SendToAddressWithMemo) |
                                        TransactionFilterProxy::TYPE(TransactionRecord::SendToOther) );
-    typeWidget->addItem(tr("To yourself"), TransactionFilterProxy::TYPE(TransactionRecord::SendToSelf));
+    typeWidget->addItem(tr("To yourself"), TransactionFilterProxy::TYPE(TransactionRecord::SendToSelf) |
+                                           TransactionFilterProxy::TYPE(TransactionRecord::SendToSelfWithMemo) );
     typeWidget->addItem(tr("Mined"), TransactionFilterProxy::TYPE(TransactionRecord::Generated));
     typeWidget->addItem(tr("Other"), TransactionFilterProxy::TYPE(TransactionRecord::Other));
     typeWidget->setObjectName("typeComboBox");
