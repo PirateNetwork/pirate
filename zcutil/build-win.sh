8d9c5e01e5c13ba2e686921d43599e34cff35f58
cryptoforge <cryptoforge.cc@gmail.com>
2023-06-11 23:03:41 -0700

    fix windows build

--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-export HOST=x86_64-w64-mingw32
+HOST=x86_64-w64-mingw32
 CXX=x86_64-w64-mingw32-g++-posix
 CC=x86_64-w64-mingw32-gcc-posix
+PREFIX="$(pwd)/depends/$HOST"
 
 set -eu -o pipefail
 
-UTIL_DIR="$(dirname "$(readlink -f "$0")")"
-BASE_DIR="$(dirname "$(readlink -f "$UTIL_DIR")")"
-PREFIX="$BASE_DIR/depends/$HOST"
+set -x
+cd "$(dirname "$(readlink -f "$0")")/.."
 
-cd $BASE_DIR/depends
-make HOST=$HOST V=1 NO_QT=1 "$@"
-cd $BASE_DIR
+cd depends/ && make HOST=$HOST V=1
+cd ../
 
 ./autogen.sh
-CONFIG_SITE=$BASE_DIR/depends/$HOST/share/config.site CXXFLAGS="-DPTW32_STATIC_LIB -DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread" ./configure --prefix=$PREFIX --host=$HOST --enable-static --disable-shared --with-gui=no
-sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' configure
 
-make "$@" make V=1  pirated.exe pirate-cli.exe pirate-tx.exe
+CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site \
+    CXXFLAGS="-DPTW32_STATIC_LIB -DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread -g0 -O2" \
+    ./configure --prefix="${PREFIX}" --host=x86_64-w64-mingw32 --enable-static --disable-shared \
+    --with-gui=no --disable-bip70 --enable-tests=no
+
+sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' configure
+cd src/
+CC="${CC}" CXX="${CXX}" make "$@" V=1
