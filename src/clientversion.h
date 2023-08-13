f971474f6d6461d65354bcefd1dd978026efbd6c
Cryptoforge <cryptoforge.cc@gmail.com>
2023-08-13 15:33:31 -0700

    set version 5.7.5

--- src/clientversion.h
+++ src/clientversion.h
@@ -32,8 +32,8 @@
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 5
 #define CLIENT_VERSION_MINOR 7
-#define CLIENT_VERSION_REVISION 4
-#define CLIENT_VERSION_BUILD 54
+#define CLIENT_VERSION_REVISION 5
+#define CLIENT_VERSION_BUILD 50
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
--- zcutil/create-deb-packages.sh
+++ zcutil/create-deb-packages.sh
@@ -1,4 +1,4 @@
-export APP_VERSION="5.7.4"
+export APP_VERSION="5.7.5"
 
 echo -n "Building amd64 deb..........."
 debdir=bin/pirate-qt-ubuntu1804-v$APP_VERSION
