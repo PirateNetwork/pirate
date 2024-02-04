c1ad20c195217540f577a4e2859ac3c449becfc2
Cryptoforge <cryptoforge.cc@gmail.com>
2024-02-03 19:26:31 -0800

    Komodo to Pirate

--- src/init.cpp
+++ src/init.cpp
@@ -1546,9 +1546,9 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
     try {
         static boost::interprocess::file_lock lock(pathLockFile.string().c_str());
         if (!lock.try_lock())
-            return InitError(strprintf(_("Cannot obtain a lock on data directory %s. Komodo is probably already running."), strDataDir));
+            return InitError(strprintf(_("Cannot obtain a lock on data directory %s. Pirate is probably already running."), strDataDir));
     } catch(const boost::interprocess::interprocess_exception& e) {
-        return InitError(strprintf(_("Cannot obtain a lock on data directory %s. Komodo is probably already running.") + " %s.", strDataDir, e.what()));
+        return InitError(strprintf(_("Cannot obtain a lock on data directory %s. Pirate is probably already running.") + " %s.", strDataDir, e.what()));
     }
 
 #ifndef _WIN32
