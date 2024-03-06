177ba906e11ded448ad2b07c641b3910c16ae1ca
DeckerSU <deckersu@protonmail.com>
2024-03-06 15:54:26 +0100

    fix messages in fetch-params.sh, fetch-params-alt.sh

--- zcutil/fetch-params-alt.bat
+++ zcutil/fetch-params-alt.bat
@@ -4,24 +4,24 @@ cd %THIS_DIR%
 IF NOT EXIST "%APPDATA%"\ZcashParams (
 MKDIR "%APPDATA%"\ZcashParams
 )
-IF NOT EXIST "%APPDATA%"\ZcashParams\sprout-proving.key (
-    ECHO Downloading Zcash trusted setup sprout-proving.key, this may take a while ...
-    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://komodoplatform.com/downloads/sprout-proving.key -O "%APPDATA%"\ZcashParams\sprout-proving.key
-)
-IF NOT EXIST "%APPDATA%"\ZcashParams\sprout-verifying.key (
-    ECHO Downloading Zcash trusted setup sprout-verifying.key, this may take a while ...
-    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://komodoplatform.com/downloads/sprout-verifying.key -O "%APPDATA%"\ZcashParams\sprout-verifying.key
-)
+@REM IF NOT EXIST "%APPDATA%"\ZcashParams\sprout-proving.key (
+@REM     ECHO Downloading Zcash trusted setup sprout-proving.key, this may take a while ...
+@REM     .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://komodoplatform.com/downloads/sprout-proving.key -O "%APPDATA%"\ZcashParams\sprout-proving.key
+@REM )
+@REM IF NOT EXIST "%APPDATA%"\ZcashParams\sprout-verifying.key (
+@REM     ECHO Downloading Zcash trusted setup sprout-verifying.key, this may take a while ...
+@REM     .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://komodoplatform.com/downloads/sprout-verifying.key -O "%APPDATA%"\ZcashParams\sprout-verifying.key
+@REM )
 IF NOT EXIST "%APPDATA%"\ZcashParams\sapling-spend.params (
-    ECHO Downloading Zcash trusted setup sprout-proving.key, this may take a while ...
+    ECHO Downloading Zcash trusted setup sapling-spend.params, this may take a while ...
     .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://komodoplatform.com/downloads/sapling-spend.params -O "%APPDATA%"\ZcashParams\sapling-spend.params
 )
 IF NOT EXIST "%APPDATA%"\ZcashParams\sapling-output.params (
-    ECHO Downloading Zcash trusted setup sprout-verifying.key, this may take a while ...
+    ECHO Downloading Zcash trusted setup sapling-output.params, this may take a while ...
     .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://komodoplatform.com/downloads/sapling-output.params -O "%APPDATA%"\ZcashParams\sapling-output.params
 )
 IF NOT EXIST "%APPDATA%"\ZcashParams\sprout-groth16.params (
-    ECHO Downloading Zcash trusted setup sprout-verifying.key, this may take a while ...
+    ECHO Downloading Zcash trusted setup sprout-groth16.params, this may take a while ...
     .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://komodoplatform.com/downloads/sprout-groth16.params -O "%APPDATA%"\ZcashParams\sprout-groth16.params
 )
 goto :EOF
