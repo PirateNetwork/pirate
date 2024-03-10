05b83954dd865d9745194cfd34eacb1255a2c497
DeckerSU <deckersu@protonmail.com>
2024-03-10 11:47:35 +0100

    build: revert darwin_debug_CFLAGS to -g -O0

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -37,7 +37,7 @@ darwin_CXXFLAGS=$(darwin_CFLAGS)
 darwin_release_CFLAGS=-O2
 darwin_release_CXXFLAGS=$(darwin_release_CFLAGS)
 
-darwin_debug_CFLAGS=-O1
+darwin_debug_CFLAGS=-g -O0
 darwin_debug_CXXFLAGS=$(darwin_debug_CFLAGS)
 
 darwin_native_binutils=native_cctools
