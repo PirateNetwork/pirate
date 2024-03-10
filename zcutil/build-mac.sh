6b8683af1a45e21361ab8200a86ea41dcbdb9624
DeckerSU <deckersu@protonmail.com>
2024-03-10 21:32:27 +0100

    build: mac remove -enable-static --disable-shared from scripts

--- zcutil/build-mac.sh
+++ zcutil/build-mac.sh
@@ -48,7 +48,9 @@ make "$@" -C ./depends/ V=1 NO_QT=1 NO_PROTON=1
 ./autogen.sh
 
 CPPFLAGS="-I$PREFIX/include -arch x86_64" LDFLAGS="-L$PREFIX/lib -arch x86_64 -Wl,-no_pie" \
-CXXFLAGS="-arch x86_64 -I$PREFIX/include -fwrapv -fno-strict-aliasing -Wno-deprecated-declarations -Wno-deprecated-builtins -Wno-enum-constexpr-conversion -Wno-unknown-warning-option -Werror -Wno-error=attributes -g" \
-./configure --prefix="${PREFIX}" --with-gui=no "$HARDENING_ARG" "$LCOV_ARG" "$DEBUGGING_ARG" --enable-static --disable-shared
+CXXFLAGS="-arch x86_64 -I$PREFIX/include -fwrapv -fno-strict-aliasing \
+-Wno-deprecated-declarations -Wno-deprecated-builtins -Wno-enum-constexpr-conversion \
+-Wno-unknown-warning-option -Werror -Wno-error=attributes -g" \
+./configure --prefix="${PREFIX}" --with-gui=no "$HARDENING_ARG" "$LCOV_ARG" "$DEBUGGING_ARG"
 
 make "$@" NO_GTEST=1 STATIC=1
