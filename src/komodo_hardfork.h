dd85d5dea665dd456c8b6d7d3c50dbecb35d507e
DeckerSU <deckersu@protonmail.com>
2024-05-16 21:15:25 +0200

    Add S8 pubkeys (placeholder) and HF estimate

--- src/komodo_hardfork.h
+++ src/komodo_hardfork.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstdint>
 
-#define NUM_KMD_SEASONS 8
+#define NUM_KMD_SEASONS 9
 #define NUM_KMD_NOTARIES 64
 
 extern const uint32_t nStakedDecemberHardforkTimestamp; //December 2019 hardfork
@@ -19,8 +19,11 @@ extern const int32_t nS6HardforkHeight;   // dPoW Season 6, Fri Jun 24 2022
 extern const uint32_t nS7Timestamp;       // dPoW Season 7, Fri Jun 30 2023 13:37:33 GMT+0000
 extern const int32_t nS7HardforkHeight;   // dPoW Season 7, Fri Jun 30 2023
 
-static const uint32_t KMD_SEASON_TIMESTAMPS[NUM_KMD_SEASONS] = {1525132800, 1563148800, nStakedDecemberHardforkTimestamp, nS4Timestamp, nS5Timestamp, nS6Timestamp, nS7Timestamp, 1951328000};
-static const int32_t KMD_SEASON_HEIGHTS[NUM_KMD_SEASONS] = {814000, 1444000, nDecemberHardforkHeight, nS4HardforkHeight, nS5HardforkHeight, nS6HardforkHeight, nS7HardforkHeight, 8113400};
+extern const uint32_t nS8Timestamp;       // dPoW Season 8, Sat Sep 14 2024 13:37:33 GMT+0000
+extern const int32_t nS8HardforkHeight;   // dPoW Season 8, Sat Sep 14 2024
+
+static const uint32_t KMD_SEASON_TIMESTAMPS[NUM_KMD_SEASONS] = {1525132800, 1563148800, nStakedDecemberHardforkTimestamp, nS4Timestamp, nS5Timestamp, nS6Timestamp, nS7Timestamp, nS8Timestamp, 1951328000};
+static const int32_t KMD_SEASON_HEIGHTS[NUM_KMD_SEASONS] = {814000, 1444000, nDecemberHardforkHeight, nS4HardforkHeight, nS5HardforkHeight, nS6HardforkHeight, nS7HardforkHeight, nS8HardforkHeight, 8113400};
 
 extern char NOTARYADDRS[64][64];
 extern char NOTARY_ADDRESSES[NUM_KMD_SEASONS][64][64];
