033e8378fc7a37e4b127b071a13b860791e5fdf1
Cryptoforge <cryptoforge.cc@gmail.com>
2024-08-31 13:57:55 -0700

    update libsodium to 1.0.20

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -1,10 +1,11 @@
 package=libsodium
-$(package)_version=1.0.18
+$(package)_version=1.0.20
 $(package)_download_path=https://download.libsodium.org/libsodium/releases
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=6f504490b342a4f8a4c4a02fc9b866cbef8622d5df4e5452b46be121e46636c1
+$(package)_sha256_hash=ebb65ef6ca439333c2bb41a0c1990587288da07f6c7fd07cb3a18cc18d30ce19
 $(package)_dependencies=
 $(package)_config_opts=
+$(package)_config_opts_aarch64=--build==$(build_prefix)
 
 define $(package)_set_vars
   $(package)_build_env=DO_NOT_UPDATE_CONFIG_SCRIPTS=1
