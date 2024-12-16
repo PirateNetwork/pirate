e6741564f72c4f08d1c3ccc4eec9c1506b1b96ae
Cryptoforge <cryptoforge.cc@gmail.com>
2024-12-16 07:46:40 -0800

    update komodo-test

--- src/test-komodo/main.cpp
+++ src/test-komodo/main.cpp
@@ -10,6 +10,26 @@ int main(int argc, char **argv) {
     assert(init_and_check_sodium() != -1);
     ECC_Start();
     ECCVerifyHandle handle;  // Inits secp256k1 verify context
+
+    //Load the Sapling parameters
+    boost::filesystem::path sapling_spend = ZC_GetParamsDir() / "sapling-spend.params";
+    boost::filesystem::path sapling_output = ZC_GetParamsDir() / "sapling-output.params";
+    boost::filesystem::path sprout_groth16 = ZC_GetParamsDir() / "sprout-groth16.params";
+
+    static_assert(
+        sizeof(boost::filesystem::path::value_type) == sizeof(codeunit),
+        "librustzcash not configured correctly");
+    auto sapling_spend_str = sapling_spend.native();
+    auto sapling_output_str = sapling_output.native();
+    auto sprout_groth16_str = sprout_groth16.native();
+
+    librustzcash_init_zksnark_params(
+        reinterpret_cast<const codeunit*>(sprout_groth16_str.c_str()),
+        sprout_groth16_str.length(),
+        true
+    );
+
+
     SetupNetworking();
     SelectParams(CBaseChainParams::REGTEST);
     chainName = assetchain(); // KMD by default
