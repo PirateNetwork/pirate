12dc4e7f4698176b60c9362183a0f1cd625d90d3
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2025-06-02 05:36:59 +0300

    Define BASE64_TOOL for MinGW build

--- depends/hosts/mingw32.mk
+++ depends/hosts/mingw32.mk
@@ -3,6 +3,8 @@ mingw32_CXX=x86_64-w64-mingw32-g++-posix
 mingw32_CFLAGS=-pipe -std=c17
 mingw32_CXXFLAGS=-pipe -std=c++17
 
+BASE64_TOOL = base64 --decode # Define BASE64_TOOL for MinGW
+
 mingw32_release_CFLAGS=-g -O2
 mingw32_release_CXXFLAGS=$(mingw32_CXXFLAGS) $(mingw32_release_CFLAGS)
 
