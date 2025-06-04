ff49e3c65266b3a8d83ca877288e9617bc5d127b
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2025-06-04 04:48:47 +0300

    Updated package build path to prevent cd errors

--- depends/funcs.mk
+++ depends/funcs.mk
@@ -88,7 +88,11 @@ $(1)_staging_dir=$(base_staging_dir)/$(host)/$(1)/$($(1)_version)-$($(1)_build_i
 $(1)_staging_prefix_dir:=$$($(1)_staging_dir)$($($(1)_type)_prefix)
 $(1)_extract_dir:=$(base_build_dir)/$(host)/$(1)/$($(1)_version)-$($(1)_build_id)
 $(1)_download_dir:=$(base_download_dir)/$(1)-$($(1)_version)
+ifeq ($$($(1)_build_subdir),.)
+$(1)_build_dir:=$$($(1)_extract_dir)
+else
 $(1)_build_dir:=$$($(1)_extract_dir)/$$($(1)_build_subdir)
+endif
 $(1)_cached_checksum:=$(BASE_CACHE)/$(host)/$(1)/$(1)-$($(1)_version)-$($(1)_build_id).tar.gz.hash
 $(1)_patch_dir:=$(base_build_dir)/$(host)/$(1)/$($(1)_version)-$($(1)_build_id)/.patches-$($(1)_build_id)
 $(1)_prefixbin:=$($($(1)_type)_prefix)/bin/
