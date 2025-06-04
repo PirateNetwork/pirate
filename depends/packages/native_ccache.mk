593347c73905b4a4fbb982df54c26ed875a42141
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2025-06-04 03:53:09 +0300

    Fix native_ccache for Windows

--- depends/packages/native_ccache.mk
+++ depends/packages/native_ccache.mk
@@ -6,6 +6,15 @@ $(package)_sha256_hash=04c0af414b8cf89e541daed59735547fbfd323b1aaa983da0216f6b67
 
 define $(package)_set_vars
 $(package)_config_opts=
+$(package)_config_opts_mingw32=--host=x86_64-w64-mingw32
+$(package)_build_opts_mingw32=CC="$(build_CC)" CXX="$(build_CXX)"
+endef
+
+define $(package)_preprocess_cmds
+  cp $(BASEDIR)/config.guess $(BASEDIR)/config.sub . && \
+  $(if $(findstring mingw32,$(host_os)), \
+    autoreconf -i \
+  )
 endef
 
 define $(package)_config_cmds
@@ -13,7 +22,7 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  $(MAKE) $($(package)_build_opts_$(host_os))
 endef
 
 define $(package)_stage_cmds
