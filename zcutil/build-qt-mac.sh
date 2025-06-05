e1cb8c496cba6e6b4956ad3081ee022c0953d9ab
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2025-06-05 06:02:17 +0300

    Added macOS architecture detection and builds
    Added automatic architecture detection to macOS build scripts to properly handle both Intel x86_64 and Apple Silicon arm64 architectures and builds.

--- zcutil/build-qt-mac.sh
+++ zcutil/build-qt-mac.sh
@@ -48,13 +48,18 @@ make "$@" -C ./depends/ V=1
 
 ./autogen.sh
 
-# LDFLAGS="-static-libgcc -static-libstdc++"
-# CPPFLAGS="-I$PREFIX/include -arch x86_64" LDFLAGS="-L$PREFIX/lib -arch x86_64 -Wl,-no_pie" \
-# CXXFLAGS='-arch x86_64 -I/usr/local/Cellar/gcc\@8/8.3.0/include/c++/8.3.0/ '"-I${PREFIX}/include"' -fwrapv -fno-strict-aliasing -g0 -O2 -Wl,-undefined -Wl,dynamic_lookup' \
-# ./configure --prefix="${PREFIX}" --with-gui=no --enable-tests=no "$HARDENING_ARG" "$LCOV_ARG"
+# Detect architecture and set appropriate flags
+ARCH=$(uname -m)
+if [ "$ARCH" = "arm64" ]; then
+    # Apple Silicon (M1/M2)
+    ARCH_FLAGS="-arch arm64"
+else
+    # Intel x86_64
+    ARCH_FLAGS="-arch x86_64"
+fi
 
-CPPFLAGS="-I$PREFIX/include -arch x86_64" LDFLAGS="-L$PREFIX/lib -arch x86_64 -Wl,-no_pie" \
-CXXFLAGS="-arch x86_64 -I$PREFIX/include -fwrapv -fno-strict-aliasing \
+CPPFLAGS="-I$PREFIX/include $ARCH_FLAGS" LDFLAGS="-L$PREFIX/lib $ARCH_FLAGS -Wl,-no_pie" \
+CXXFLAGS="$ARCH_FLAGS -I$PREFIX/include -fwrapv -fno-strict-aliasing \
 -Wno-deprecated-declarations -Wno-deprecated-builtins -Wno-enum-constexpr-conversion \
 -Wno-unknown-warning-option -Werror -Wno-error=attributes -g" \
 ./configure --prefix="${PREFIX}" --disable-bip70 --with-gui=qt5 "$HARDENING_ARG" "$LCOV_ARG" "$DEBUGGING_ARG"
