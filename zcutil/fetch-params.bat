733ad676a59550cbdce5e54c4f4e43185351a880
Øswald Kardingson <164262873+OswaldKardingson@users.noreply.github.com>
2026-07-24 21:43:28 +0300

    Use GitHub release for Zcash parameters

--- zcutil/fetch-params.bat
+++ zcutil/fetch-params.bat
@@ -6,23 +6,23 @@ MKDIR "%APPDATA%"\ZcashParams
 )
 @REM IF NOT EXIST "%APPDATA%"\ZcashParams\sprout-proving.key (
 @REM     ECHO Downloading Zcash trusted setup sprout-proving.key, this may take a while ...
-@REM     .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sprout-proving.key.deprecated-sworn-elves -O "%APPDATA%"\ZcashParams\sprout-proving.key
+@REM     .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://github.com/PirateNetwork/zcash_params/releases/download/release_bd4e9a3/sprout-proving.key -O "%APPDATA%"\ZcashParams\sprout-proving.key
 @REM )
 @REM IF NOT EXIST "%APPDATA%"\ZcashParams\sprout-verifying.key (
 @REM     ECHO Downloading Zcash trusted setup sprout-verifying.key, this may take a while ...
-@REM     .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sprout-verifying.key -O "%APPDATA%"\ZcashParams\sprout-verifying.key
+@REM     .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://github.com/PirateNetwork/zcash_params/releases/download/release_bd4e9a3/sprout-verifying.key -O "%APPDATA%"\ZcashParams\sprout-verifying.key
 @REM )
 IF NOT EXIST "%APPDATA%"\ZcashParams\sapling-spend.params (
     ECHO Downloading Zcash trusted setup sapling-spend.params, this may take a while ...
-    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sapling-spend.params -O "%APPDATA%"\ZcashParams\sapling-spend.params
+    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://github.com/PirateNetwork/zcash_params/releases/download/release_bd4e9a3/sapling-spend.params -O "%APPDATA%"\ZcashParams\sapling-spend.params
 )
 IF NOT EXIST "%APPDATA%"\ZcashParams\sapling-output.params (
     ECHO Downloading Zcash trusted setup sapling-output.params, this may take a while ...
-    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sapling-output.params -O "%APPDATA%"\ZcashParams\sapling-output.params
+    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://github.com/PirateNetwork/zcash_params/releases/download/release_bd4e9a3/sapling-output.params -O "%APPDATA%"\ZcashParams\sapling-output.params
 )
 IF NOT EXIST "%APPDATA%"\ZcashParams\sprout-groth16.params (
     ECHO Downloading Zcash trusted setup sprout-groth16.params, this may take a while ...
-    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://z.cash/downloads/sprout-groth16.params -O "%APPDATA%"\ZcashParams\sprout-groth16.params
+    .\wget64.exe --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://github.com/PirateNetwork/zcash_params/releases/download/release_bd4e9a3/sprout-groth16.params -O "%APPDATA%"\ZcashParams\sprout-groth16.params
 )
 goto :EOF
 :GET_CURRENT_DIR
